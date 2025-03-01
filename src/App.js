77285497f749c54da4995ad854e36fad8d886412
gayathri <thallurigayathri@gmail.com>
2025-03-01 14:42:39 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL = "https://task-manager-backend-x2zb.onrender.com/tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    axios.get(API_URL).then((res) => setTasks(res.data));
+  }, []);
+
+  const addTask = () => {
+    axios.post(API_URL, { name: task }).then((res) => {
+      setTasks([...tasks, res.data]);
+      setTask("");
+    });
+  };
+
+  const deleteTask = (id) => {
+    axios.delete(`${API_URL}/${id}`).then(() => {
+      setTasks(tasks.filter((t) => t._id !== id));
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Manager</h1>
+      <input value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((t) => (
+          <li key={t._id}>
+            {t.name} <button onClick={() => deleteTask(t._id)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
